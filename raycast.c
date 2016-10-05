5ea762726ffe8b82a384f834bca7a38548a7cd27
Clarissac <clarissacalderon23@gmail.com>
2016-10-04 23:33:50 -0700

    Update raycast.c

--- raycast.c
+++ raycast.c
@@ -42,7 +42,7 @@ char* parse_string(FILE* json){
     buffer[1] = 0;
     return strdup(buffer);
     return buffer;
-
+}
 void skip_ws(FILE* json){
     int c = fgetc(json);
     while(isspace(c)){
@@ -56,11 +56,18 @@ if (strcmp(type, "camera") == 0) {
     else if(strcmp(type, "sphere") == 0){
         else if (strcmp(type, "plane") == 0) {
             else {
+                fprintf(stderr, "Error: Unknown type");
                 exit(1);
             }
         }
     }
 }
+
+
+
+
+
+
 
 int main(){
     FILE* json = fopen("input.json", "r");
@@ -78,8 +85,44 @@ int main(){
     exit(0);
     }
 
-
+skip_ws(json)
+c = next_c(json);
+if(c == '}'){
+    break;
 }
+else if (c == ','){
+    skip_ws(json);
+    char* key = next_string(json);
+    skip_ws(json);
+    expect_c(json,':');
+    skip_ws(json);
+    if(strcmp(key, "width") == 0) || (strcmp(key,"height") == 0) || (strcmp(key,"radius") == 0){
+        double value = next_number(json);}
+        else{
+            fprintf(stderr, "Error: Unknown property.";)}
+            else{
+                fprintf(stderr, "Error: Unexpected value on line %d\n", line);
+                exit(1);
+            }
+        }
+    }
+
+double next_number(FILE* json){
+double value;
+fscanf(json, "%f", &value);
+return value;}
+
+double* next_vector(FILE* json){
+    double* V = malloc(3*sizeof(double));
+    v[0] = next_number(json);
+    skip_ws(json);
+    expect_c(json, ',');
+    skip_ws(json);
+    v[1]=next number(json);
+    skip_ws(json);
+    expect_c(json, ',');
+    skip_ws(json);
+    v[1] = next_number(json);}
 
 
 
@@ -98,4 +141,4 @@ void write_p3(Pixel* buffer, int width, int height, char* filename) {
 
 
 
-}
+
