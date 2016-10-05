a01bc69c14c6527e76cfa88f37a677f74b6ff28d
Clarissac <clarissacalderon23@gmail.com>
2016-10-04 22:42:58 -0700

    Update raycast.c

--- raycast.c
+++ raycast.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 
 typedef struct {
@@ -22,6 +23,36 @@ typedef struct {
     } plane;
   };
 } Object;
+
+
+
+void skip_ws(FILE* json){
+    int c = fgetc(json);
+    while(isspace(c)){
+        c = fgetc(json);}
+        ungetc(c, json);
+}
+
+int main(){
+    FILE* json = fopen("input.json", "r");
+    skip_ws(json);
+    int c = fgetc(json);
+    if (c! = '[') {
+        fprintf(stderr, "Error: File must begin with list");
+        fclose(json);
+        exit(1)
+    }
+    skip_ws(json);
+    
+    c = fgetc(json);
+    fclose(json);
+    exit(o);
+    }
+
+}
+
+
+
 void write_p3(Pixel* buffer, int width, int height, char* filename) {
       FILE* fh = fopen(filename, "w");
       fprintf("P3 %d %d 255 ");
@@ -29,12 +60,12 @@ void write_p3(Pixel* buffer, int width, int height, char* filename) {
             fprintf("%d %d %d ", buffer[i].r, buffer[i].g, buffer[i].b);
       }
       fclose(fh);
-        
-      
+
+
 }
-  
-  
-  
-  
- 
+
+
+
+
+
 }
